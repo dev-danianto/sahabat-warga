f6cb178b2eaea62197ed787bf382c90b597137d4
dev-danianto <dev.danianto@gmail.com>
2025-08-27 00:28:07 +0700

    v1 without working detail

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,15 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import {Platform, View} from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+//Icon ION
+import { Ionicons } from "@expo/vector-icons";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -26,20 +29,52 @@ export default function TabLayout() {
           default: {},
         }),
       }}>
+        <Tabs.Screen
+            name="home"
+            options={{
+                title: 'Home',
+                tabBarIcon: ({ color, size}) => (
+                    <Ionicons name="home-sharp" size={size + 4} color="" />
+                )
+            }}
+        />
+        <Tabs.Screen
+            name="kesehatan"
+            options={{
+                title: 'Kesehatan',
+                tabBarIcon: ({ color, size}) => (
+                    <Ionicons name="medical" size={size + 4} color="" />
+                )
+            }}
+        />
       <Tabs.Screen
-        name="index"
+        name="darurat"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+            title: 'Darurat',
+            tabBarIcon: ({ color, size}) => (
+                <Ionicons name="alert-circle" size={size + 4} color="" />
+            )
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+        <Tabs.Screen
+            name="info"
+            options={{
+                title: 'Informasi',
+                tabBarIcon: ({ color, size}) => (
+                    <Ionicons name="logo-ionic" size={size + 4} />
+                )
+            }}
+        />
+        <Tabs.Screen
+            name="kerja"
+            options={{
+                title: 'Kerja',
+                tabBarIcon: ({ color, size}) => (
+                    <Ionicons name="briefcase" size={size + 4} />
+                )
+            }}
+        />
+
     </Tabs>
   );
 }
